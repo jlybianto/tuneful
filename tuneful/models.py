414f72c02f7789c07bb5c1517628987aa7f74830
jlybianto <jesse.lybianto@gmail.com>
2015-06-01 05:31:40 +0000

    Create Song and File model database

--- tuneful/models.py
+++ tuneful/models.py
@@ -7,3 +7,28 @@ from sqlalchemy.orm import relationship
 from tuneful import app
 from database import Base, engine
 
+class Song(Base):
+    __tablename__ = "songs"
+    id = Column(Integer, primary_key=True)
+    file = relationship("File", uselist=False, backref="song")
+    
+    def asDictionary(self):
+        return {
+            "id": 1,
+            "file": {
+                "id": 7,
+                "name": "Shady_Grove.mp3"
+            }
+        }
+
+class File(Base):
+    ___tablename__ = "files"
+    id = Column(Integer, primary_key=True)
+    filename = Column(String(1024))
+    song_id = Column(Integer, ForeignKey("songs.id"))
+    
+    def asDictionary(self):
+        return {
+            "id": 7,
+            "name": "Shady_Grove.mp3"
+        }
